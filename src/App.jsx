d0cd68af90550de9c90b787089e4d44281a7cefe
KevinJoseph <kevramos95@gmail.com>
2023-05-07 19:08:47 -0500

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import Item from './components/Item';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Secondary from './components/Secondary';
+import Item from './components/Item';
 import { useData } from './hooks/useData';
 
-function App() {
+const App = () => {
   const { data, setData } = useData();
-
   return (
-    <>
-    <Secondary data={data} setData={setData}></Secondary>
-    <Item data={data} setData={setData}></Item>
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/home">
+          {() => <Item data={data} setData={setData} />}
+        </Route>
+        <Route exact path="/secondary">
+          {() => <Secondary data={data} setData={setData} />}
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
